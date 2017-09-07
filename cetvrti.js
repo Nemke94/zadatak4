9c60eaaa1f4cd304d49e95cb81860a2a3e9d26b6
Nemanja Grujić <nemanjagrujic1994@gmail.com>
2017-09-08 01:24:10 +0200

    Add files via upload

---
+++ cetvrti.js
@@ -0,0 +1,7 @@
+var stanjeRacuna = 325;
+
+var proizvod = 14;
+
+	rezultat = stanjeRacuna / proizvod;
+	var moguDaKupim = Math.floor(rezultat);
+	console.log("Broj proizvoda koje mozemo da kupimo: " + moguDaKupim)
